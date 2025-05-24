b6e67a976621d07f4350301489c13c2c3b423bba
Rahul-taak <rtaak786@gmail.com>
2025-05-24 12:25:49 +0530

    tailwind setup done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   // Load environment variables
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     base: '/',
     server: {
       historyApiFallback: true,
