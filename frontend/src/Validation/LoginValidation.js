40f3b85428bf72be042f948470cb65269f5c4c5c
Rahul-taak <rtaak786@gmail.com>
2025-06-05 23:18:53 +0530

    sapce added

--- frontend/src/Validation/LoginValidation.js
+++ frontend/src/Validation/LoginValidation.js
@@ -3,7 +3,6 @@ function Validation(values) {
   const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   // Validations for Email ===============
-
   if (values.email === "") {
     error.email = "Email should not be empty";
   } else if (!email_pattern.test(values.email)) {
@@ -11,7 +10,6 @@ function Validation(values) {
   }
 
   // Validations for Password ===============
-
   if (values.password === "") {
     error.password = "Password should not be empty";
   }
