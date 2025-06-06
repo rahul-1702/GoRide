2731b30b0de48bfc8887a3fa6455779c8358655f
Rahul-taak <rtaak786@gmail.com>
2025-06-06 23:21:30 +0530

    space added

--- frontend/src/Validation/LoginValidation.js
+++ frontend/src/Validation/LoginValidation.js
@@ -3,6 +3,7 @@ function Validation(values) {
   const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   // Validations for Email ===============
+  
   if (values.email === "") {
     error.email = "Email should not be empty";
   } else if (!email_pattern.test(values.email)) {
@@ -10,6 +11,7 @@ function Validation(values) {
   }
 
   // Validations for Password ===============
+
   if (values.password === "") {
     error.password = "Password should not be empty";
   }
